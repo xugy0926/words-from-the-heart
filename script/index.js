ba902f30e1d3f900b4e498a654655dec2c4ec128
young <young_xu@qq.com>
2017-08-07 23:02:25 +0800

    add script.

---
+++ script/index.js
@@ -0,0 +1,38 @@
+// 引入fs模块
+const fs = require('fs');
+const fse = require('fs-extra');
+
+// 异步方式读取words-from-the-heart的所有文件
+fs.readdir('../', function(err, files) {
+  if (err) {
+    console.log('readdir error.');
+    return;
+  }
+
+  console.log('----read begin----');
+  console.log(files);
+  console.log('----read end----')
+
+  //把含有json的文件名过滤出来
+  var jsonFiles = [];
+  for (var i = 0; i < files.length; i++) {
+    if (files[i].includes("json")) {
+      jsonFiles.push(files[i]);
+    }
+  }
+
+  console.log('----filter begin----');
+  console.log(jsonFiles);
+  console.log('----filter end----');
+
+  // 检查错误的json文件
+  for (var i = 0; i < jsonFiles.length; i++) {
+    fse.readJson('../' + jsonFiles[i])
+      .then(obj => {
+        // do nothing.
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+});
